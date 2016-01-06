dbdc31a63ffc0cbc1f2006642f3bf7883c40e85f
Emily Zhou <eyzhou@andrew.cmu.edu>
2016-01-06 12:34:28 -0500

    Added scrolling images in Recipes tab

---
+++ app/src/main/java/com/eyzhou/zest/RowItem.java
@@ -0,0 +1,7 @@
+package com.eyzhou.zest;
+
+/**
+ * Created by eyzhou on 1/6/16.
+ */
+public class RowItem {
+}
---
+++ app/src/main/java/com/eyzhou/zest/TabFragment4.java
@@ -0,0 +1,16 @@
+package com.eyzhou.zest;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+/**
+ * Created by eyzhou on 1/6/16.
+ */
+public class TabFragment4 extends Fragment{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab_fragment_4, container, false);
+    }
+}
